9ce2a9318855a22a73e6e532274ddf2a2400c9d4
brendan.mcmahon.bloomerang <90283736+brendanjeffreymcmahon@users.noreply.github.com>
2024-05-07 16:42:21 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,33 @@
+import './style.css'
+
+const circle = document.getElementById('inner-wheel');
+let active = false;
+let currentAngle = 0;
+let startAngle = 0;
+let rotationOrigin = { x: 0, y: 0 };
+
+function calculateAngle(event) {
+    const dx = event.clientX - rotationOrigin.x;
+    const dy = event.clientY - rotationOrigin.y;
+    return Math.atan2(dy, dx);
+}
+
+circle.addEventListener('touchstart', function (event) {
+    const rect = circle.getBoundingClientRect();
+    rotationOrigin.x = rect.left + rect.width / 2;
+    rotationOrigin.y = rect.top + rect.height / 2;
+    startAngle = calculateAngle(event.touches[0]) - currentAngle;
+    active = true;
+}, false);
+
+circle.addEventListener('touchmove', function (event) {
+    if (!active) return;
+    event.preventDefault(); // Prevent scrolling and zooming
+    const angle = calculateAngle(event.touches[0]);
+    currentAngle = angle - startAngle;
+    circle.style.transform = `rotate(${currentAngle}rad)`;
+}, false);
+
+circle.addEventListener('touchend', function (event) {
+    active = false;
+}, false);
